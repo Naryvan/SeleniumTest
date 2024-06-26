ebe471aef644c9725284af85ca5f5c45bba3b40b
DanyloKozak <danilo.kozak2003@gmail.com>
2024-06-26 20:18:36 +0300

    fixed test 3

--- src/test/java/TestImplementation.java
+++ src/test/java/TestImplementation.java
@@ -2,6 +2,10 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -44,10 +48,13 @@ public class TestImplementation {
         driver.get("https://www.gmail.com");
         driver.manage().window().maximize();
         driver.findElement(By.id("identifierId")).sendKeys("selenium11037@gmail.com");
-        driver.findElement(By.className("VfPpkd-RLmnJb")).click();
-        driver.findElement(By.className("whsOnd zHQkBf")).sendKeys("Qwerty123_");
-        driver.findElement(By.className("VfPpkd-RLmnJb")).click();
-        assertThat(driver.getTitle(), containsString("inbox"));
+        driver.findElement(By.id("identifierNext")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
+        driver.findElement(By.name("Passwd")).sendKeys("Qwerty123_");
+        driver.findElement(By.id("passwordNext")).click();
+        wait.until(ExpectedConditions.titleContains("Inbox"));
+        assertThat(driver.getTitle(), containsString("Inbox"));
     }
 
 }
